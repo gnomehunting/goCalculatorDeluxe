ff4364ab1523ff58015da3a3a8f5e893e92b7e8c
Vovandopole <vdenisov2006@gmail.com>
2024-04-24 01:21:29 +0500

    commit

---
+++ RUN_ME.bat
@@ -0,0 +1,8 @@
+cd /d "%~dp0"
+
+start go run .\orchestra\orchestra.go 8080
+start go run .\agent\agent.go 8081
+start go run .\agent\agent.go 8082
+start go run .\agent\agent.go 8083
+start go run .\agent\agent.go 8084
+start go run .\agent\agent.go 8085
